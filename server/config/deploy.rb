1a9f60a14d8582025e99f0bb97c92e2edc78638d
Nathanael Burt <nathanael.burt@gmail.com>
2015-04-10 17:33:28 -0600

    add unicorn

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -60,5 +60,12 @@ namespace :deploy do
     end
   end
 
+  after 'deploy:publishing', 'deploy:restart'
+  namespace :deploy do
+    task :restart do
+      invoke 'unicorn:restart'
+    end
+  end
+
 end
 
