732299cbaf6351fbc2f67aa3437b1a41831319be
Nathanael Burt <nathanael.burt@gmail.com>
2015-04-13 21:27:08 -0600

    symlink secrets.yml

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/rails'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml .env}
+set :linked_files, %w{config/database.yml .env config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
